01eb6f140521adbe35b2570fabfd311b096c777a
hazto <zach.hassler@gmail.com>
2023-01-14 16:02:21 -0800

    notes

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import { harvest } from './harvest';
 const port = 3034;
 const app = express();
 
-
+// may need to break goblin challenges into a grid
+// take stock of each image in each slot, make best guess on what might be a goblin and what might not be a goblin
+// 2 strikes then bail
+// law of averages it will guess right
+// need to define what constitutes 'gobliness'
+// use machine learning?  teach it what's a goblin and what isn't?  oh... sounds like fun!!!
 
 harvest();
